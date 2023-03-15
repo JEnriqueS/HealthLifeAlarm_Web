cff9b22e0c8feb702d5005f1a613ad2cffff9cde
Jorge Enrique Suárez González <je.suarezg1@uniandes.edu.co>
2023-03-15 17:37:01 -0500

    Proyecto base sin acciones móviles

--- HealthLifeAlarm/src/app/app.module.ts
+++ HealthLifeAlarm/src/app/app.module.ts
@@ -8,17 +8,10 @@ import { LoginModule } from './login/login.module';
 import { RecuperarClaveModule } from './recuperarClave/recuperarClave.module';
 import { Mis_habitosModule } from './mis_habitos/mis_habitos.module';
 import { Mi_progresoModule } from './mi_progreso/mi_progreso.module';
-import { AlarmasModule } from './alarmas/alarmas.module';
 import { Eliminar_cuentaModule } from './eliminar_cuenta/eliminar_cuenta.module';
-import { Crear_cuentaModule } from './crear_cuenta/crear_cuenta.module';
 import { Actualizar_datosModule } from './actualizar_datos/actualizar_datos.module';
 import { MatDialogModule } from '@angular/material/dialog';
-import { Crear_alarmaModule } from './crear_alarma/crear_alarma.module';
 import { EstadisticasModule } from './estadisticas/estadisticas.module';
-import { Agregar_ringtoneModule } from './agregar_ringtone/agregar_ringtone.module';
-import { Modificar_alarmaModule } from './modificar_alarma/modificar_alarma.module';
-import { Establecer_metaModule } from './establecer_meta/establecer_meta.module';
-import { Crear_habitoModule } from './crear_habito/crear_habito.module';
 
 @NgModule({
   declarations: [
@@ -32,17 +25,10 @@ import { Crear_habitoModule } from './crear_habito/crear_habito.module';
     RecuperarClaveModule,
     Mis_habitosModule,
     Mi_progresoModule,
-    AlarmasModule,
     Eliminar_cuentaModule,
-    Crear_cuentaModule,
     Actualizar_datosModule,
     MatDialogModule,
-    EstadisticasModule,
-    Agregar_ringtoneModule,
-    Crear_alarmaModule,
-    Modificar_alarmaModule,
-    Establecer_metaModule,
-    Crear_habitoModule
+    EstadisticasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
