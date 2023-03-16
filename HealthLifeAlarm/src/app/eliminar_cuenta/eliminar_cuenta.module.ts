9464387df4c08637c9b83e1618533ad6a7ea6456
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-03-16 00:26:38 -0500

    Pagina eliminar cuenta

--- HealthLifeAlarm/src/app/eliminar_cuenta/eliminar_cuenta.module.ts
+++ HealthLifeAlarm/src/app/eliminar_cuenta/eliminar_cuenta.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Eliminar_cuentaComponent } from './eliminar_cuenta.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+
+const uiModules = [
+  MatIconModule
+]
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    uiModules
   ],
   declarations: [Eliminar_cuentaComponent],
-  exports: [Eliminar_cuentaComponent]
+  exports: [Eliminar_cuentaComponent, uiModules]
 })
 export class Eliminar_cuentaModule { }
